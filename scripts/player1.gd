bd4641f1295ff8b067ec83a9c494f5b3e8a50d1a
novikpavel <novikovpavel031@gmail.com>
2024-10-14 18:13:56 -0500

    Added Player1 and Player2; Added attack to player1

--- scripts/player1.gd
+++ scripts/player1.gd
@@ -6,6 +6,12 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var is_attacking: bool = false #Flag to check whether the player is attacking
+
+#Play attack animation
+func attack() -> void:
+	is_attacking = true
+	animated_sprite.play("attack")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,6 +20,9 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("player1_jump") and is_on_floor():
+		if is_attacking == true:
+			is_attacking = false
+			animated_sprite.stop()
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0, 1
@@ -27,9 +36,9 @@ func _physics_process(delta: float) -> void:
 		
 		#Play animations
 	if is_on_floor():
-		if direction == 0:
+		if direction == 0 and not is_attacking:
 			animated_sprite.play("idle")
-		else:
+		elif direction > 0 or direction < 0 and not is_attacking:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
@@ -39,5 +48,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_just_pressed("player1_attack"):
+		attack()
 
 	move_and_slide()
+	
+	
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "attack":
+		is_attacking = false # Reset attack state after animation completes
